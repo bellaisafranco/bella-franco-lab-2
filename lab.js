adac4632c19104642728a9e99274f4c2a0e4ae68
bellaisafranco <isabella.franco@edu.sait.ca>
2024-11-07 18:00:45 -0700

    im sorry

---
+++ lab.js
@@ -0,0 +1,39 @@
+sheep.addEventListener('click',function(){
+	sheep.innerHTML = "Bighorn Sheep";
+  
+	document.getElementById('banner').src="sheep.webp";
+	document.getElementById('banner').alt="two mountain sheep stand on the mountain";
+    document.getElementById('sheep').style
+})
+
+bear.addEventListener('click',function(){
+	bear.innerHTML = "Black Bear";
+  
+	document.getElementById('banner').src="bear.webp";
+	document.getElementById('banner').alt="two mountain sheep stand on the mountain";
+    document.getElementById('sheep').style
+})
+
+moose.addEventListener('click',function(){
+	moose.innerHTML = "Moose";
+  
+	document.getElementById('banner').src="moose.webp";
+	document.getElementById('banner').alt="two mountain sheep stand on the mountain";
+    document.getElementById('sheep').style
+})
+
+wolf.addEventListener('click',function(){
+	wolf.innerHTML = "Wolf";
+  
+	document.getElementById('banner').src="wolf.webp";
+	document.getElementById('banner').alt="two mountain sheep stand on the mountain";
+    document.getElementById('sheep').style
+})
+
+elk.addEventListener('click',function(){
+	elk.innerHTML = "Elk";
+  
+	document.getElementById('banner').src="elk.webp";
+	document.getElementById('banner').alt="two mountain sheep stand on the mountain";
+   
+})
